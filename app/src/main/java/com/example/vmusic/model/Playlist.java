a872c2691e1de10b47163e72256d689780d3505a
XuanHung <hungtxhe171405@fpt.edu.vn>
2025-06-24 00:04:11 +0700

    playlist

---
+++ app/src/main/java/com/example/vmusic/model/Playlist.java
@@ -0,0 +1,12 @@
+package com.example.vmusic.model;
+public class Playlist {
+    public String title;
+    public String description;
+    public int imageResId;
+
+    public Playlist(String title, String description, int imageResId) {
+        this.title = title;
+        this.description = description;
+        this.imageResId = imageResId;
+    }
+}
