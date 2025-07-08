79e67bdb3597950d2d201dc371baaac98ddb8b87
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-07-08 14:50:11 +0700

    crud (#17)
    Co-authored-by: TranBaBinh <binhtbhe173244@fpt.edu.vn>

--- app/src/main/java/dao/SongDao.java
+++ app/src/main/java/dao/SongDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Transaction;
@@ -22,10 +23,26 @@ public interface SongDao {
     @Query("SELECT * FROM songs WHERE songId = :id")
     LiveData<SongWithGenres> getSongWithGenreLive(int id);
 
+    @Query("SELECT * FROM songs WHERE songId = :songid")
+    LiveData<Song> getSongById(int songid);
+
+    @Transaction
+    @Query("SELECT s.* FROM songs s " +
+            "INNER JOIN SongGenreCrossRef sg ON s.songId = sg.songId " +
+            "WHERE sg.genreId = :genreId")
+    LiveData<List<Song>> getSongsByGenreId(int genreId);
+
+    @Query("SELECT * FROM songs WHERE songId = :id")
+    Song getSongById2(int id);
+
+
     @Insert
     void insert(Song song);
 
     @Update
     void updateSong(Song song);
+
+    @Delete
+    void deleteSong(Song song);
 
 }
