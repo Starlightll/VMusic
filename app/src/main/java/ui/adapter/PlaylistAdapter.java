b31a1a0559a1a6a8d138cb2335db9e27f5e7fcfe
XuanHung <hungtxhe171405@fpt.edu.vn>
2025-07-11 00:32:23 +0700

    commit tạm

---
+++ app/src/main/java/ui/adapter/PlaylistAdapter.java
@@ -0,0 +1,55 @@
+package ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vmusic.R;
+
+import models.Playlist;
+import java.util.List;
+
+
+public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ViewHolder> {
+
+    private List<Playlist> playlists;
+
+    public PlaylistAdapter(List<Playlist> playlists) {
+        this.playlists = playlists;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_playlist, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Playlist playlist = playlists.get(position);
+        holder.title.setText(playlist.title);
+        holder.description.setText(playlist.description);
+        holder.image.setImageResource(playlist.imageResId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return playlists.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title, description;
+        ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            image = itemView.findViewById(R.id.image);
+        }
+    }
+}
