afabf785fd78ac4e1a8d9365135e8f442f87e754
TranBaBinh <binhtbhe173244@fpt.edu.vn>
2025-07-15 02:22:57 +0700

    .

--- app/src/main/java/com/example/vmusic/ui/fragment/SearchTabFragment.java
+++ app/src/main/java/com/example/vmusic/ui/fragment/SearchTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.vmusic.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,15 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.OptIn;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.media3.common.util.UnstableApi;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vmusic.R;
+import com.example.vmusic.service.PlaybackService;
+import com.example.vmusic.viewmodel.PlayerViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,11 +43,12 @@ public class SearchTabFragment extends Fragment {
 
     private List<Genre> fullGenreList = new ArrayList<>();
 
+    @OptIn(markerClass = UnstableApi.class)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_tab, container, false);
-
+        PlayerViewModel playerViewModel = new ViewModelProvider(requireActivity()).get(PlayerViewModel.class);
         // Genre RecyclerView
         RecyclerView genreRecyclerView = view.findViewById(R.id.rv_genres);
         genreRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
@@ -53,7 +59,16 @@ public class SearchTabFragment extends Fragment {
         RecyclerView songRecyclerView = view.findViewById(R.id.rv_songs);
         songRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         songAdapter = new SongAdapter(song -> {
-            Toast.makeText(getContext(), "Clicked: " + song.name, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(requireContext(), PlaybackService.class);
+            intent.putExtra("url", song.getAudioUrl());
+            intent.putExtra("name", song.getName());
+            intent.putExtra("artist", song.getArtist());
+            intent.putExtra("image", song.getImage());
+            requireContext().startService(intent);
+
+            playerViewModel.setCurrentSong(song);
+            playerViewModel.setIsPlaying(true);
+            songViewModel.increaseListenCount(song);
         });
 
         songRecyclerView.setAdapter(songAdapter);
